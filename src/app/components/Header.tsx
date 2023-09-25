d2c7040d69065cf1de599905c48a1fd1d1a66d5d
paulo <paulo@suaid.io>
2023-09-25 16:48:28 -0300

    implementando lógica de alteração no cabeçalho global

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,28 +1,28 @@
-import Image from "next/image";
-import Link from "next/link";
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
 import appLogo from './../../../public/logo-fantasy-magist.svg'
+import { usePathname } from 'next/navigation'
 
 export default function Header() {
-  return (
-    <header className='fixed z-50 w-screen px-28'>
-    <div className='flex text-white justify-between items-center'>
-      <Link href={'/'}>
-      <Image
-        src={appLogo}
-        alt='Fantasy Magist'
-        width={245}
-        height={50}
-      />
-      </Link>
-      <div className='flex gap-8 items-center'>
-        <Link href='characters'>
-          Characters
-        </Link>
-        <h2 className='px-4 py-2 border-white border-[1px] rounded-md'>
-          Sign In
-        </h2>
-      </div>
-    </div>
-  </header>
-  )
+	const loginOrRegisterLink = usePathname() !== '/sign/login'
+
+	return (
+		<header className='fixed z-50 w-screen px-28'>
+			<div className='flex text-white justify-between items-center'>
+				<Link href={'/'}>
+					<Image src={appLogo} alt='Fantasy Magist' width={245} height={50} />
+				</Link>
+				<div className='flex gap-8 items-center'>
+					<Link href='/characters'>Characters</Link>
+					<Link
+						href={loginOrRegisterLink ? '/sign/login' : '/sign/register'}
+						className='px-4 py-2 border-white border-[1px] rounded-md'
+					>
+						{loginOrRegisterLink ? 'Sign In' : 'Sign Up'}
+					</Link>
+				</div>
+			</div>
+		</header>
+	)
 }
