dc03b5807b7c752928522c1dde12f0530deb274c
paulo <paulo@suaid.io>
2023-11-24 16:48:30 -0300

    fixes

--- src/app/(authenticated)/characters/components/CharacterCreationModal.tsx
+++ src/app/(authenticated)/characters/components/CharacterCreationModal.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Modal from '@/app/components/Modal'
+import TransparentTextInput from '@/app/components/input/TransparentTextInput'
 import { Dispatch, SetStateAction, useState } from 'react'
 
 export default function CharacterCreationModal({
@@ -11,7 +12,38 @@ export default function CharacterCreationModal({
 }) {
 	return (
 		<Modal state={isOpen} dispatchFunction={setIsOpen}>
-			<div>abre e fecha</div>
+			<div className='flex flex-col gap-6 px-6 pb-8 pt-2'>
+				<div className='flex flex-col gap-2 text-center px-4'>
+					<h2 className='text-2xl font-semibold border-b-2 border-b-gray-200 py-2'>
+						New Character
+					</h2>
+					<h3 className='opacity-75'>First Steps</h3>
+				</div>
+
+				<form>
+					<div className='flex flex-col gap-6 justify-around items-center'>
+						<div className='flex items-end gap-6'>
+							<TransparentTextInput label={{ content: 'Name' }} />
+
+							<select className='py-3 px-4 h-fit rounded-md shadow-sm'>
+								<option defaultChecked>Species</option>
+							</select>
+						</div>
+
+						<div className='flex items-end gap-6'>
+							<TransparentTextInput
+								label={{ content: 'Level' }}
+								type='number'
+								numberRange={{ min: 1, max: 20 }}
+							/>
+
+							<select className='py-3 px-4 h-fit rounded-md shadow-sm'>
+								<option defaultChecked>Classes</option>
+							</select>
+						</div>
+					</div>
+				</form>
+			</div>
 		</Modal>
 	)
 }
--- src/app/components/input/TransparentTextInput.tsx
+++ src/app/components/input/TransparentTextInput.tsx
@@ -3,13 +3,48 @@ interface ITransparentTextInputProps {
 	onChange?: (event: React.FormEvent<any>) => any
 	placeholder?: string
 	value?: string
+	type?: 'text' | 'password' | 'number'
+	numberRange?: {
+		min: number
+		max: number
+	}
 	extraClasses?: string
 	id?: string
+	label?: {
+		content: string
+		position?: 'TOP' | 'BOTTOM'
+	}
 }
 
 export default function TransparentTextInput(
 	props: ITransparentTextInputProps
 ) {
+	if (props.label?.content) {
+		if (!props.label.position) {
+			props.label.position = 'TOP'
+		}
+
+		const direction =
+			props.label.position === 'TOP' ? 'flex-col' : 'flex-col-reverse'
+
+		return (
+			<div className={`flex ${direction}`}>
+				<label>{props.label.content}</label>
+				<input
+					type={props.type || 'text'}
+					min={props.numberRange?.min || undefined}
+					max={props.numberRange?.max || undefined}
+					className={`bg-transparent border-b-[1px] border-gray-400 ${props.extraClasses}`}
+					value={props.value}
+					id={props.id}
+					placeholder={props.placeholder}
+					onClick={props.onClick}
+					onChange={props.onChange}
+				/>
+			</div>
+		)
+	}
+
 	return (
 		<input
 			type='text'
