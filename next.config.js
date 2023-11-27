90e3860f2a9fe79b6580b2268444d98650c9efcf
Paulo Valleriote <paulomv29@outlook.com>
2023-11-27 08:27:00 -0300

    changes in next configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    PUBLIC_URL: "http://localhost:3000",
     API_URL: "http://localhost:8080"
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: process.env.PUBLIC_URL,
+      bodySizeLimit: '2mb'
+    }
   }
 }
 
